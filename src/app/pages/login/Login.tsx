11c4cc356b18656cc7beaa5557699a41d438f38c
DerickSena <derick_sena13@outlook.com>
2023-11-01 17:42:38 -0300

    feat: add login page and form

--- src/app/pages/login/Login.tsx
+++ src/app/pages/login/Login.tsx
@@ -1,5 +1,27 @@
+import { useState } from "react";
+
 export const Login = () => {
+    const [email,setEmail] = useState('');
+    const [senha,setSenha] = useState('');
+    const handleEntrar =()=>{
+        console.log(email)
+        console.log(senha)
+    }
     return(
-        <p>Login</p>
-    )
+        <div>
+           <form>
+                <label>
+                    <span>Email</span>
+                    <input value={email} onChange={e => setEmail(e.target.value)}/>
+                </label>
+                <label>
+                    <span>Senha</span>
+                    <input type="password" value={senha} onChange={e => setSenha(e.target.value)}/>
+                </label>
+                <button type="button" onClick={handleEntrar}>
+                    Entrar
+                </button>
+           </form>
+        </div>
+    );
 }
